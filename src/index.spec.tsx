5f344e4ba80f90c49e928424a4fdab2d84761be3
Tuan Anh Pham <tuanh118@gmail.com>
2018-04-06 12:15:03 -0700

    initialized with typescript

---
+++ src/index.spec.tsx
@@ -0,0 +1,11 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from './App'
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <App />
+  )
+  expect(tree).toBeDefined()
+})
